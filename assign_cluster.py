0a04150e68fea3dfe1dd512eb160214dd2c4f3ad
Armin-Hajibeygi <hajibeygi.armin@gmail.com>
2025-03-22 14:37:27 +0100

    feat: add date

--- assign_cluster.py
+++ assign_cluster.py
@@ -11,28 +11,40 @@ def get_papers_name(path):
 
 
 def assign_paper_to_person(
-    sheet: gsheet_connector.Sheet, topic: str, papers: list[str], person: str
+    sheet: gsheet_connector.Sheet,
+    topic: str,
+    papers: list[str],
+    person: str,
+    date: str = None,
 ):
     sheet.set_worksheet_id(person)
     row_index = sheet.get_last_row_index() + 1
     sheet.insert_paper_vertical(row_index, 1, papers)
     topic_list = [topic] * len(papers)
     sheet.insert_paper_vertical(row_index, 2, topic_list)
+    if date:
+        date_list = [date] * len(papers)
+        sheet.insert_paper_vertical(row_index, 3, date_list)
 
 
 def assign_paper_to_people(
-    sheet: gsheet_connector.Sheet, topic: str, papers: list[str], people: list[str]
+    sheet: gsheet_connector.Sheet,
+    topic: str,
+    papers: list[str],
+    people: list[str],
+    date: str = None,
 ):
     for person in people:
-        assign_paper_to_person(sheet, topic, papers, person)
+        assign_paper_to_person(sheet, topic, papers, person, date)
 
 
 if __name__ == "__main__":
     sheet_name = "LLM Group Paper Assignment"
     path = "/Users/armin/Desktop/HEC/Research/Data Analytics Group/LLM fairness"
     topic = path.split("/")[-1]
-    people = ["Armin"]
+    people = ["Armin", "Stef", "Sajjad"]
+    date = "Mar 22"
 
     sheet = gsheet_connector.Sheet(sheet_name)
     papers = get_papers_name(path)
-    assign_paper_to_people(sheet, topic, papers, people)
+    assign_paper_to_people(sheet, topic, papers, people, date)
